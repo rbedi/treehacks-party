f0822e49cf957335c7e328bde0a918162d5d68ec
Jason Teplitz <jteplitz602@gmail.com>
2022-05-02 21:56:33 -0700

    more cleanup

--- webpage.js
+++ webpage.js
@@ -1,15 +1,35 @@
 setTimeout('$(".cover_image_covid").css("opacity",.9)',1000);
 
+function scrollToId(id){
+  $('html,body').animate({scrollTop: $("#"+id).offset().top - 10},'slow');
+}
+
+function scrollToClass(className){
+  $('html,body').animate({scrollTop: $("."+className).offset().top - 10},'slow');
+}
+
+function hash(string) {
+  const utf8 = new TextEncoder().encode(string);
+  return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((bytes) => bytes.toString(16).padStart(2, '0'))
+      .join('');
+
+    hashed_pw = "1a04417ebb8f626ba1302cf77ac1cb212d3c80d61133b75d8d224d2274688005"
+    return hashHex;
+  });
+}
+
 $(document).ready(function() {  
 
   $(".password-input").focus();
 
   $('.preload').load(function() {  
       $(".cover_image_covid").css("opacity",.9);
- });  
-  setTimeout('$(".cover_image_covid").css("opacity",.9)',1000);
-});
+  });  
 
+  setTimeout('$(".cover_image_covid").css("opacity",.9)',1000);
 
   $(".learn2").click(function() {
     scrollToClass("faq")
@@ -19,18 +39,15 @@ $(document).ready(function() {
     scrollToClass("scroll-apply")
   });
 
-
-var input = document.getElementById("password-input");
-input.addEventListener("keyup", function(event) {
-// Number 13 is the "Enter" key on the keyboard
-if (event.keyCode === 13) {
-  // Cancel the default action, if needed
-  event.preventDefault();
-  // Trigger the button element with a click
-  document.getElementById("let-me-in").click();
-}
-});
-
+  $("#password-input").keyup((event) => {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Can't let this happen
+      event.preventDefault();
+      // This is right, right?
+      $("#let-me-in").click();
+    }
+  });
  
   $(".let-me-in").click(function() {
 
@@ -49,27 +66,4 @@ if (event.keyCode === 13) {
       }
     }
   });
-
-
-
-function scrollToId(id){
-$('html,body').animate({scrollTop: $("#"+id).offset().top - 10},'slow');
-}
-
-function scrollToClass(className){
-$('html,body').animate({scrollTop: $("."+className).offset().top - 10},'slow');
-}
-
-function hash(string) {
-const utf8 = new TextEncoder().encode(string);
-return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((bytes) => bytes.toString(16).padStart(2, '0'))
-    .join('');
-
-  hashed_pw = "1a04417ebb8f626ba1302cf77ac1cb212d3c80d61133b75d8d224d2274688005"
-  return hashHex;
 });
-}
-
